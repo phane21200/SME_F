a084721cd36e53a75af1b3c7d53da6342bcdcacf
stephane mennessier <stephane_mennessier@yahoo.fr>
2023-03-07 17:00:06 +0100

    dfg

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,6 +1,15 @@
 <template>
-  <q-page>
-    <h5>Ma liste</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bp-white">
+      <q-item v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="color" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Teal</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -8,6 +17,26 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'A_Faire'
+  name: 'A_Faire',
+
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'Choix 1',
+          done: false
+        },
+        {
+          title: 'Choix 2',
+          done: false
+        },
+        {
+          title: 'Choix 3',
+          done: false
+        }
+      ]
+
+    }
+  }
 })
 </script>
